2d1494199cecb15b4dad4067d0b8a3a57ca4f7c3
mahmud aremu <aremumahmud2003@gmail.com>
2024-05-11 00:57:34 +0100

    added secrets

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const io = socketIo(server, {
 
 app.use(express.json())
 app.use(express.urlencoded()) // Connect to MongoDB
-mongoose.connect(process.mongoURI || 'mongodb://localhost:27017/clipboard', {
+mongoose.connect(process.env.mongoURI || 'mongodb://localhost:27017/clipboard', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
